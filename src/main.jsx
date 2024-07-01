a2044f4748bceb5088ed4a35b2da55627537496b
kanishq73 <94435952+kanishq73@users.noreply.github.com>
2024-07-02 01:59:58 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { createBrowserRouter ,RouterProvider } from 'react-router-dom'
-import Twoplayer from './Components/Twoplayer/Twoplayer.jsx'
-import Home from './Components/Home/Home.jsx'
-import Computer from './Components/Computer/Computer.jsx'
-
-const router= createBrowserRouter([
-  {
-    path:'/',
-    element:<App/>,
-    children:[
-      {
-        path:'/',
-        element:<Home/>
-      },
-      {
-        path:'/Pass-N-Play',
-        element:<Twoplayer/>
-      },
-      {
-        path:'/Computer',
-        element:<Computer/>
-      },
-      
-    ]
-  }
-])
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import Twoplayer from './Components/Twoplayer/Twoplayer.jsx';
+import Home from './Components/Home/Home.jsx';
+import Computer from './Components/Computer/Computer.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}/> 
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="Pass-N-Play" element={<Twoplayer />} />
+          <Route path="Computer" element={<Computer />} />
+        </Route>
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
-)
+);
